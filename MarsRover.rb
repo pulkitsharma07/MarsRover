78f913699e8cf48edc428a3608d836b1ea723964
Pulkit Sharma <avastpulkit@gmail.com>
2016-07-13 15:08:59 +0530

    initial commit

---
+++ MarsRover.rb
@@ -0,0 +1,77 @@
+module MarsRover
+
+DIRECTIONS = (0..3).to_a.freeze
+DIRECTION_LITERAL = "NESW"
+GRID_MAX = []
+
+	class Rover
+
+		def initialize(x,y,dir)
+			@x,@y = x.to_i,y.to_i
+			@curr_dir = DIRECTION_LITERAL.index(dir.to_s) || 0 #default direction is zero
+		end
+
+		def print_pos
+			puts "#{@x} #{@y} #{DIRECTION_LITERAL[@curr_dir]}\n"
+		end
+
+		def command_seq(cmd_seq)
+			cmd_seq.each do |cmd|
+				command(cmd)
+			end
+		end
+
+		def command(input)
+			
+			case input
+			when "L"
+				turn_left
+			when "R"	
+				turn_right
+			when "M"
+				move_forward
+			else
+				raise "Invalid Move : #{input}"
+			end
+			
+			keep_in_bounds
+			#puts "After #{input} "
+			#print_pos
+		end
+
+		private
+
+		def keep_in_bounds
+			@x = [0,@x].max
+			@y = [0,@y].max
+			@x = [GRID_MAX[0],@x].min
+			@y = [GRID_MAX[1],@y].min
+		end
+
+		def turn_left
+			@curr_dir = (@curr_dir - 1) % 4;
+		end
+
+		def turn_right
+			@curr_dir = (@curr_dir + 1) % 4;
+		end
+
+		def move_forward
+
+			case @curr_dir
+			when 0
+				@y += 1
+			when 1
+				@x += 1
+			when 2
+				@y -= 1
+			when 3
+				@x -= 1
+			end
+		end
+	end
+
+	def self.set_bounds(bounds)
+		GRID_MAX.replace(bounds)
+	end
+end
---
+++ script.rb
@@ -0,0 +1,25 @@
+require_relative 'MarsRover.rb'
+
+MarsRover.set_bounds(gets.chomp.split.map(&:to_i))
+
+rovers = []
+
+while line = gets
+	next if line == "\n"
+
+	line = line.chomp.split;
+	rovers << MarsRover::Rover.new(line[0],line[1],line[2])
+
+	line = gets.chomp.split('')
+	rovers.last.command_seq(line)
+end
+
+rovers.each do |rover| 
+	rover.print_pos
+end
+
+
+
+
+
+
