e85e1182d9a0103be8cd70c9ce1b3776374b0311
Pulkit Sharma <avastpulkit@gmail.com>
2016-07-13 19:29:55 +0530

    Add some more basic tests

--- spec.rb
+++ spec.rb
@@ -1,11 +1,107 @@
 require 'rspec'
 require_relative 'MarsRover.rb'
 
+DIR = "NESW".freeze
+
 describe "Rover" do
 
 	before do 
 		MarsRover.set_bounds([10,10])
 	end
+
+	context "rotations" do
+
+		it "should go from N to E on turning Right" do 
+			rover = MarsRover::Rover.new(0,0,"N")
+
+			rover.command("R")
+			expect(DIR[rover.curr_dir]).to eql("E")
+		end
+
+		it "should go from E to S on turning Right" do 
+			rover = MarsRover::Rover.new(0,0,"E")
+
+			rover.command("R")
+			expect(DIR[rover.curr_dir]).to eql("S")
+		end
+
+		it "should go from S to W on turning Right" do 
+			rover = MarsRover::Rover.new(0,0,"S")
+
+			rover.command("R")
+			expect(DIR[rover.curr_dir]).to eql("W")
+		end
+
+		it "should go from W to N on turning Right" do 
+			rover = MarsRover::Rover.new(0,0,"W")
+
+			rover.command("R")
+			expect(DIR[rover.curr_dir]).to eql("N")
+		end
+
+		it "should go from N to E on turning Right" do 
+			rover = MarsRover::Rover.new(0,0,"N")
+
+			rover.command("R")
+			expect(DIR[rover.curr_dir]).to eql("E")
+		end
+
+		it "should go from W to S on turning Left" do 
+			rover = MarsRover::Rover.new(0,0,"W")
+
+			rover.command("L")
+			expect(DIR[rover.curr_dir]).to eql("S")
+		end
+
+
+	end
+
+	context "Movement" do
+
+		it "Should move Left if facing West" do
+			rover = MarsRover::Rover.new(5,5,"W")
+
+			rover.command("M")
+			expect(rover.x).to eql(4)
+			expect(rover.y).to eql(5)
+
+		end	
+
+
+		it "Should move Up if facing North" do
+			rover = MarsRover::Rover.new(5,5,"N")
+
+			rover.command("M")
+
+			expect(rover.x).to eql(5)
+			expect(rover.y).to eql(6)
+
+		end	
+
+
+		it "Should move Down if facing South" do
+			rover = MarsRover::Rover.new(5,5,"S")
+
+			rover.command("M")
+
+			expect(rover.x).to eql(5)
+			expect(rover.y).to eql(4)
+
+		end	
+
+
+
+		it "Should move Right if facing East" do
+			rover = MarsRover::Rover.new(5,5,"E")
+
+			rover.command("M")
+
+			expect(rover.x).to eql(6)
+			expect(rover.y).to eql(5)
+
+		end	
+	end
+
 	context "should face at the same direction after 4 rotations" do
 
 		it "on left side" do	
@@ -18,7 +114,7 @@ describe "Rover" do
 				rover.command("R")
 				rover.command("R")
 				
-				expect(rover.curr_dir).to eql("NESW".index(dir))
+				expect(DIR[rover.curr_dir]).to eql(dir)
 			end
 
 		end
@@ -33,7 +129,7 @@ describe "Rover" do
 				rover.command("L")
 				rover.command("L")
 				
-				expect(rover.curr_dir).to eql("NESW".index(dir))
+				expect(DIR[rover.curr_dir]).to eql(dir)
 			end
 
 		end
